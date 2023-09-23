6160d3614e8cbacb8950da6a491878e2c11f0ff6
gruntcole <gruntcole1998@gmail.com>
2023-09-23 15:05:28 -0400

    made seed functions to create and drop tables, and rebiuld the db

--- db/seed-data.js
+++ db/seed-data.js
@@ -1,16 +1,40 @@
 import client from "./client";
 
-export async function createTables () {
-    console.log("Starting to build tables...");
-try {
+export async function createTables() {
+  console.log("Starting to build tables...");
+  try {
     await client.query(`
     CREATE TABLE meats (
         id SERIAL PRIMARY KEY,
+
     );
-    `)
-    console.log("Tables have been built!")
-} catch (err){
-    console.log("Error building tables!")
+    `);
+    console.log("Tables have been built!");
+  } catch (err) {
+    console.log("Error building tables!");
     throw err;
+  }
 }
+
+export async function dropTables() {
+  console.log("Starting to dop tables...");
+  try {
+    await client.query(`
+        DROP TABLE meats;
+        `);
+    console.log("Tables have been dropped!");
+  } catch (err) {
+    console.log("Error dropping tables!");
+    throw err;
+  }
+}
+
+export async function rebuildDB() {
+  try {
+    await dropTables();
+    await createTables();
+  } catch (err) {
+    console.log("Error rebuilding Database!");
+    throw err;
+  }
 }
